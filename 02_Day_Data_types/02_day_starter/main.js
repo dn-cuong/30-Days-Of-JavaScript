ab6ded08eaa21e0e95d6142a04cc0b8ab9e8098d
Nguyen Dang Cuong <you@example.com>
2023-04-28 21:08:54 +0700

    Excercise 1

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,18 @@
 // this is your main.js script
+
+
+// Exercises 1:
+
+
+var challenge = "30 days of JS";
+
+console.log(challenge);
+
+console.log(challenge.length);
+
+console.log(challenge.toLocaleLowerCase());
+
+console.log(challenge.toLocaleUpperCase());
+
+
+
